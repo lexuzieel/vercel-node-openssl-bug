364836138ca2f8f9607f1fe3c7ac21d4389a6d76
Aleksei Ivanov <contact@aleksei.dev>
2020-11-03 12:07:42 +0300

    Trigger deployment

--- api/index.js
+++ api/index.js
@@ -4,26 +4,21 @@ module.exports = (req, res) => {
     let parameters = ['smime', '-help'];
     let reply = [];
 
-    try {
-        const openSSLProcess = spawn('openssl', parameters);
+    const openSSLProcess = spawn('openssl', parameters);
 
-        openSSLProcess.stdout.on('data', (data) => {
-            reply.push(data.toString());
-        });
+    openSSLProcess.stdout.on('data', (data) => {
+        reply.push(data.toString());
+    });
 
-        openSSLProcess.stderr.on('data', (data) => {
-            reply.push(data.toString());
-        });
+    openSSLProcess.stderr.on('data', (data) => {
+        reply.push(data.toString());
+    });
 
-        openSSLProcess.on('close', (code) => {
-            reply.push(`OpenSSL process ends with code ${code}`);
+    openSSLProcess.on('close', (code) => {
+        reply.push(`OpenSSL process ends with code ${code}`);
 
-            let text = reply.join('\n');
+        let text = reply.join('\n');
 
-            res.send(`<pre>${text}</pre>`)
-        });
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
+        res.send(`<pre>${text}</pre>`)
+    });
 }
